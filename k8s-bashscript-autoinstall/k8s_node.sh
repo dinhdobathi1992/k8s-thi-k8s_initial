2b2e975cae506e4742f9389d86f03d9eeefd3c0a
Angelvn Dark <vietson1@gmail.com>
2021-08-30 09:55:41 +0000

    Upload New File

---
+++ k8s-bashscript-autoinstall/k8s_node.sh
@@ -0,0 +1,12 @@
+#/bin/bash -x
+#Intalling k8s
+echo "Intalling k8s"
+yum install -y kubelet kubeadm kubectl --disableexcludes=kubernetes
+systemctl start kubelet
+systemctl enable kubelet
+
+#Join K8S Cluster
+echo "Joining K8S Cluster"
+sshpass -p "vagrant" scp -o StrictHostKeyChecking=no vagrant@kube-master:/home/vagrant/kubeadm_join_cmd.sh .
+chmod +x kubeadm_join_cmd.sh
+sh ./kubeadm_join_cmd.sh
