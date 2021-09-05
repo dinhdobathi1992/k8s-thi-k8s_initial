d8d6c47118d003dd19adf25e632e8d8fb4e40bab
linhnh90 <linh.nh@medofa.com>
2021-09-05 10:37:00 +0700

    upload terraform

---
+++ k8s-terraform/README.md
@@ -0,0 +1,14 @@
+<!-- A GCP service account key:
+- Create a service account key to enable Terraform to access your GCP account. When creating the key, use the following settings:
+    + Select the project you created in the previous step.
+    + Click "Create Service Account".
+    + Give it any name you like and click "Create".
+    + For the Role, choose "Project -> Editor", then click "Continue".
+    + Skip granting additional users access, and click "Done".
+    + After you create your service account, download your service account key.
+
+    + Select your service account from the list.
+    + Select the "Keys" tab.
+    + In the drop down menu, select "Create new key".
+    + Leave the "Key Type" as JSON.
+Click "Create" to create the key and save the key file to your system. -->
---
+++ k8s-terraform/ingress.tf
@@ -0,0 +1,44 @@
+######################################
+#            create network          #
+######################################
+resource "google_compute_network" "vpc_network" {
+  name = var.gce_network
+}
+
+######################################
+#            ingress allow           #
+######################################
+resource "google_compute_firewall" "ingress_allow_internal" {
+  name      = "${var.gce_network}-allow-internal"
+  network   = google_compute_network.vpc_network.name
+  direction = "INGRESS"
+
+  allow {
+    protocol = "icmp"
+  }
+
+  allow {
+    protocol = "tcp"
+    ports    = ["0-65535"]
+  }
+
+  allow {
+    protocol = "udp"
+    ports    = ["0-65535"]
+  }
+
+  source_ranges = ["10.128.0.0/9"]
+}
+
+resource "google_compute_firewall" "ingress_allow_ssh" {
+  name      = "${var.gce_network}-allow-ssh"
+  network   = google_compute_network.vpc_network.name
+  direction = "INGRESS"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+}
